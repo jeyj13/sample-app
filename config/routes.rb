9e77be064cac3dcc52198753615a7db147751e2b
jeyj13 <jeyj13@gmail.com>
2017-05-10 16:46:38 +0000

    added full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages/home'
-
+  
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  root 'static_pages#home'
 
-  get 'application#hello'
+  
 end
