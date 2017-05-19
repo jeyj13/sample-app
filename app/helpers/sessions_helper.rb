68e96527e43d5712a049d9c33ca43bb056bc7369
jeyj13 <jeyj13@gmail.com>
2017-05-19 14:02:47 +0000

    Finish user edit, update, destroy, and index actions. Tests now produce errors constantly.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,19 @@
 module SessionsHelper
 
-  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
   end
   def current_user
     if (user_id = session[:user_id])
@@ -20,6 +26,7 @@ module SessionsHelper
       end
     end
   end
+  
   def logged_in?
     !current_user.nil?
   end
@@ -32,5 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
