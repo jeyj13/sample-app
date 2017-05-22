cc0f16a517fd35cf98376403f332224c3671f43e
jeyj13 <jeyj13@gmail.com>
2017-05-22 18:56:33 +0000

    Added authentication tests- currently failing

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -21,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
