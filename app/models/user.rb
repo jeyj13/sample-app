1013800ac91a33a6814c6d6efc504200815e9482
jeyj13 <jeyj13@gmail.com>
2017-05-30 15:39:26 +0000

    added account activation, has unsolved errors from tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,16 @@ class User < ApplicationRecord
   def downcase_email
       self.email = email.downcase
   end
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   def create_activation_digest
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,13 +26,27 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+ # config.action_mailer.raise_delivery_errors = true
+ # config.action_mailer.delivery_method = :test
+ # host = 'rails-tutorial-jeyoung.c9users.io'
+  #config.action_mailer.default_url_options = { host: 'example.com' }
+ # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  #config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
