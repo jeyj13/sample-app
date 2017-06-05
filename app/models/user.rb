a115dd2c263f50d334dc4cfcbeda0e319ad62b20
jeyj13 <jeyj13@gmail.com>
2017-06-05 14:40:17 +0000

    Beginning 13.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,9 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private
 
     # Converts email to all lower-case.
